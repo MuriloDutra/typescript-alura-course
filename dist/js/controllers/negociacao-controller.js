e7a2cfd2a1d73b5f9d087ae46f0f69790b3e6e20
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-16 22:27:18 -0300

    Adding types to variables and methods

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,23 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        return new Negociacao(date, parseInt(this.inputQuantidade.value), parseFloat(this.inputValor.value));
+    }
+    limparFormulario() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
+    }
+}
