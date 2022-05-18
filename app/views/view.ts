17e91eea3bee018062149983582ce59e64c531bb
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-18 17:46:41 -0300

    Adding optional parameter in View.ts

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,12 +1,19 @@
 export abstract class View<T> {
     protected elemento: HTMLElement;
+    private escapar: boolean = false;
 
-    constructor(seletor: string){
+    constructor(seletor: string, escapar?: boolean){
         this.elemento = document.querySelector(seletor)
+        if(escapar) this.escapar = escapar
     }
 
     public update(model: T): void{
-        this.elemento.innerHTML = this.template(model)
+        let template = this.template(model)
+        if(this.escapar){
+            /<script>[\s\S]*?<script>/
+            template = template.replace(/<script>[\s\S]*?<\/script>/, "")
+        }
+        this.elemento.innerHTML = template
     }
 
     protected abstract template(model: T): string;
