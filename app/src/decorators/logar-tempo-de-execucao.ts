9881454c33be5c809717f99664781f527910c0cf
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-19 21:09:46 -0300

    User can choose log in seconds or milliseconds

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos:boolean = false) {
     return function(
         target: any,
         propertyKey: string,
@@ -7,10 +7,12 @@ export function logarTempoDeExecucao() {
         const metodoOriginal = descriptor.value
         
         descriptor.value = function (...args: Array<any> ){
+            let divisor = emSegundos ? 1000 : 1 
+            let unidade = emSegundos ? "segundos": "milisegundos"
             const t1 = performance.now()
             const ret = metodoOriginal.apply(this, args)//biding
             const t2 = performance.now()
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`)
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/divisor} ${unidade}`)
             ret
         }
 
