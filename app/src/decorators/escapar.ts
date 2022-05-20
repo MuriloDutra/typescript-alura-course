2238cea7234decf45b0a34c2bea715416022d3aa
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-19 21:57:52 -0300

    new decorator escapar

---
+++ app/src/decorators/escapar.ts
@@ -0,0 +1,16 @@
+export function escapar(
+    target: string,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+){
+    const metodoOriginal = descriptor.value
+    descriptor.value = function (...args: any[]){
+        let ret = metodoOriginal.apply(this, args)
+        if(typeof ret === "string"){
+            console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`)
+            ret = ret.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        return ret
+    }
+    return descriptor
+}
