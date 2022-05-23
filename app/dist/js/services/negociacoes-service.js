97db6613716aa590e008d15b84cc0d37743b60dc
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-05-23 15:43:34 -0300

    Creating NegociacoesService

---
+++ app/dist/js/services/negociacoes-service.js
@@ -0,0 +1,12 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacoesService {
+    obterNegociacoesDoDia() {
+        return fetch("http://localhost:8080/dados")
+            .then(res => res === null || res === void 0 ? void 0 : res.json())
+            .then((dados) => {
+            return dados.map(item => {
+                return new Negociacao(new Date(), item.vezes, item.montante);
+            });
+        });
+    }
+}
